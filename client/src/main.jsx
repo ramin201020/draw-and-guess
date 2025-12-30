dbe26369524da6c3b09f41bdecaa9f69c9e720d8
ilan2004 <ilanusman03@gmail.com>
2025-12-30 19:46:49 +0530

    Fix React JSX runtime - use automatic JSX transform

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SocketProvider } from './socket/SocketProvider';
 import { LandingPage } from './pages/LandingPage';
 import { RoomPage } from './pages/RoomPage';
 import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <SocketProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/room/:roomId" element={<RoomPage />} />
-      </Routes>
-    </BrowserRouter>
-  </SocketProvider>
+  <StrictMode>
+    <SocketProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/room/:roomId" element={<RoomPage />} />
+        </Routes>
+      </BrowserRouter>
+    </SocketProvider>
+  </StrictMode>
 );
